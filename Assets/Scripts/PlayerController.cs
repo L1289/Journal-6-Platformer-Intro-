ffd2858ccc750c38dfc9c2412cd5c35c3f8921d0
L1289 <lallsuk@sheridancollege.ca>
2024-12-14 16:09:02 -0500

    player movement and animations
    Managed to get player movement working for left and right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,6 +8,14 @@ public class PlayerController : MonoBehaviour
     {
         left, right
     }
+
+    public float speed;
+    public Rigidbody2D playerBody;
+    private Vector2 velocity;
+    public float acceleration = 3f;
+    public float postiveSpeedLimit = 6f;
+    public float negativeSpeedLimit = 6f;
+    private FacingDirection direction = FacingDirection.right;
 
     // Start is called before the first frame update
     void Start()
@@ -16,29 +26,47 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        IsWalking();
         // The input from the player needs to be determined and
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
+        playerInput.x = Input.GetAxisRaw("Horizontal");
         MovementUpdate(playerInput);
+
+        playerBody.velocity = velocity;
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        if (playerInput.x != 0)
+        {
+            //moves the player and updates their postion based on the current speed they are going at 
+            velocity.x += acceleration * playerInput.x * Time.deltaTime;
+            velocity.x = Mathf.Clamp(velocity.x, -negativeSpeedLimit, postiveSpeedLimit);
+        }
+
 
     }
 
     public bool IsWalking()
     {
-        return false;
+        if (velocity.x != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     public bool IsGrounded()
     {
-        return false;
+        return true;
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        return direction;
     }
 }
